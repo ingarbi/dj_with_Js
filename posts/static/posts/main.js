98b5a2300ac1eb9a556691c6f17ea065be221634
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-02-16 17:33:12 +0300

    Posts cards

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -23,15 +23,29 @@ $.ajax({
   success: function (response) {
     console.log(response);
     const data = response.data;
-    setTimeout(() =>{
-      spinnerBox.classList.add('not-visible')
+    setTimeout(() => {
+      spinnerBox.classList.add("not-visible");
       data.forEach((element) => {
         postsBox.innerHTML += `
-          ${element.title} - <b>${element.body}</b> <br>
+          <div class="card mb-2" >
+            <div class="card-body">
+              <h5 class="card-title">${element.title}</h5>
+              <p class="card-text">${element.body}</p>
+            </div>
+            <div class="card-footer">
+              <div class="row">
+                <div class="col-1">
+                  <a href="#" class="btn btn-primary">Details</a>
+                </div>
+                <div class="col-1">
+                  <a href="#" class="btn btn-primary">Like</a>
+                </div>
+              </div>
+            </div>
+          </div>
         `;
       });
-    }, 150)
-
+    }, 150);
   },
   error: function (err) {
     console.log("error", err);
