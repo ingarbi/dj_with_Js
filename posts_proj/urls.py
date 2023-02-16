0d098acac07457546d5d6fcc05091c9a4d8db16c
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-02-16 14:06:42 +0300

    First template, js, view

--- posts_proj/urls.py
+++ posts_proj/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #     path('posts/', include('posts.urls')),
+    path('', include('posts.urls', namespace='posts')),
     #     path('profiles/', include('profiles.urls')),
 ]
 
