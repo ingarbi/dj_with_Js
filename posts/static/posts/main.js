70511ac4ea5b2c793618cb2ea6a4b1ee77b37626
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-02-16 17:17:18 +0300

    Spinner

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,15 +1,16 @@
 const helloWorldBox = document.getElementById("hello-world");
 const postsBox = document.getElementById("posts-box");
+const spinnerBox = document.getElementById("spinner-box");
 
 // helloWorldBox.textContent = 'Hello world'
 // helloWorldBox.innerHTML = "Hello <b>world</b>";
 
 $.ajax({
-  type: 'GET',
+  type: "GET",
   url: "/hello-world/",
   success: function (response) {
-    console.log('succes', response.text);
-    helloWorldBox.textContent = response.text
+    console.log("succes", response.text);
+    helloWorldBox.textContent = response.text;
   },
   error: function (err) {
     console.log("error", err);
@@ -17,19 +18,22 @@ $.ajax({
 });
 
 $.ajax({
-  type: 'GET',
-  url: '/data/',
-  success: function(response){
-    console.log(response)
-    const data = response.data
-    data.forEach(element => {
-      postsBox.innerHTML += `
-        ${element.title} - <b>${element.body}</b> <br>
-      `
-    });
-   
+  type: "GET",
+  url: "/data/",
+  success: function (response) {
+    console.log(response);
+    const data = response.data;
+    setTimeout(() =>{
+      spinnerBox.classList.add('not-visible')
+      data.forEach((element) => {
+        postsBox.innerHTML += `
+          ${element.title} - <b>${element.body}</b> <br>
+        `;
+      });
+    }, 150)
+
   },
   error: function (err) {
     console.log("error", err);
   },
-})
+});
