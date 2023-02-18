85bbdf72688d93071c30c264b02a4afd86e1605f
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-02-18 21:34:13 +0300

    Create Post in Modal Post

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,8 +1,13 @@
-const helloWorldBox = document.getElementById("hello-world");
 const postsBox = document.getElementById("posts-box");
 const spinnerBox = document.getElementById("spinner-box");
 const loadBtn = document.getElementById("load-btn");
 const endBox = document.getElementById("end-box");
+
+const postForm = document.getElementById("post-form");
+const title = document.getElementById("id_title");
+const body = document.getElementById("id_body");
+const csrf = document.getElementsByName("csrfmiddlewaretoken");
+const alertBox = document.getElementById('alert-box')
 
 const getCookie = (name) => {
   let cookieValue = null;
@@ -108,6 +113,57 @@ loadBtn.addEventListener("click", () => {
   spinnerBox.classList.remove("not-visible");
   visible += 3;
   getData();
+});
+
+postForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $.ajax({
+    type: "POST",
+    url: "",
+    data: {
+      'csrfmiddlewaretoken': csrf[0].value,
+      'title': title.value,
+      'body': body.value,
+    },
+    success: function (response) {
+      postsBox.insertAdjacentElement(
+        "afterbegin",
+        `
+      <div class="card mb-2" >
+      <div class="card-body">
+        <h5 class="card-title">${response.title}</h5>
+        <p class="card-text">${response.body}</p>
+      </div>
+      <div class="card-footer">
+        <div class="row">
+          <div class="col-1">
+            <a href="#" class="btn btn-primary">Details</a>
+          </div>
+          <div class="col-2">
+            <form class="like-unlike-forms" data-form-id="${response.id}">
+
+            <button href="#" class="btn btn-primary" id="like-unlike-${
+              response.id
+            }">Like(0)</button>
+              </form>
+          </div>
+        </div>
+      </div>
+    </div>
+      `
+      );
+      likeUnlikePosts()
+      $('#addPostModal').modal('hide')
+      handleAlerts('success', 'New post added')
+      postForm.reset()
+
+    },
+    error: function (error) {
+      console.log(error)
+      handleAlerts('danger', 'Smt went wrong')
+    },
+  });
 });
 
 getData();
--- posts/views.py
+++ posts/views.py
@@ -14,6 +14,12 @@ def post_list_and_create(request):
             instance = form.save(commit=False)
             instance.author = author
             instance.save()
+            return JsonResponse({
+                'title': instance.title,
+                'body': instance.body,
+                'author': instance.author.user.username,
+                'id': instance.id,
+            })
     context = {
         'form': form,
     }
@@ -21,24 +27,25 @@ def post_list_and_create(request):
 
 
 def load_post_data_view(request, num_posts):
-    visible = 3
-    upper = num_posts
-    lower = upper - visible
-    size = Post.objects.all().count()
+    if request.is_ajax():
+        visible = 3
+        upper = num_posts
+        lower = upper - visible
+        size = Post.objects.all().count()
 
-    qs = Post.objects.all()
-    data = []
-    for obj in qs:
-        item = {
-            'id': obj.id,
-            'title': obj.title,
-            'body': obj.body,
-            'liked': True if request.user in obj.liked.all() else False,
-            'count': obj.like_count,
-            'author': obj.author.user.username
-        }
-        data.append(item)
-    return JsonResponse({'data': data[lower:upper], 'size': size})
+        qs = Post.objects.all()
+        data = []
+        for obj in qs:
+            item = {
+                'id': obj.id,
+                'title': obj.title,
+                'body': obj.body,
+                'liked': True if request.user in obj.liked.all() else False,
+                'count': obj.like_count,
+                'author': obj.author.user.username
+            }
+            data.append(item)
+        return JsonResponse({'data': data[lower:upper], 'size': size})
 
 
 def like_unlike_post(request):
