728150cc6bd3236890c2781a8bb6b1aaccc8bf4b
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-02-20 12:48:12 +0300

    Post detail Page finished

--- posts/static/posts/detail.js
+++ posts/static/posts/detail.js
@@ -1,6 +1,9 @@
 const backBtn = document.getElementById("back-btn");
+const updateBtn = document.getElementById("update-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const url = window.location.href + "data/";
 const spinnerBox = document.getElementById("spinner-box");
+const postBox = document.getElementById("post-box");
 
 backBtn.addEventListener("click", () => {
   history.back();
@@ -10,6 +13,23 @@ $.ajax({
   type: "GET",
   url: url,
   success: function (response) {
+    const data = response.data;
+    if (data.logged_in !== data.author) {
+    } else {
+      updateBtn.classList.remove("not-visible");
+      deleteBtn.classList.remove("not-visible");
+    }
+    const titleEl = document.createElement("h3");
+    titleEl.setAttribute("class", "mt-3");
+
+    const bodyEl = document.createElement("p");
+    bodyEl.setAttribute("class", "mt-1");
+
+    titleEl.textContent = data.title;
+    bodyEl.textContent = data.body;
+    postBox.appendChild(titleEl)
+    postBox.appendChild(bodyEl)
+
     spinnerBox.classList.add("not-visible");
   },
   error: function (error) {},
